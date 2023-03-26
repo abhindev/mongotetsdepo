5e3573b32ffc177e4ed9928d102ce3fbd3ad8378
Abhindev <abhindev4432@gmail.com>
2023-03-26 15:47:20 +0530

    poss

---
+++ pages/api/product/get.ts
@@ -0,0 +1,41 @@
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from 'mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Product = {
+  name: string;
+  description: string;
+  price: number;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
+
+  const client = await clientPromise;
+  const db = client.db("kalianiammas");
+
+  switch (method) {
+    case 'GET':
+      try {
+        const data = await db.collection("products").find().toArray();
+        res.status(200).json(data);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+      break;
+    case 'POST':
+      try {
+        const product: Product = body;
+        const result = await db.collection("products").insertOne(product);
+        res.status(201).json({ message: "Product created successfully", id: result.insertedId });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).json({ message: `Method ${method} Not Allowed` });
+  }
+};
