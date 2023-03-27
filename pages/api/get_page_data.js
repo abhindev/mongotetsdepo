d24d6b02e52f30ecefbb81f80e8ce151d4c0005c
Abhindev <abhindev4432@gmail.com>
2023-03-27 14:47:23 +0530

    error

--- pages/api/get_page_data.js
+++ pages/api/get_page_data.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
       res.status(response.status).json(responseData);
     } catch (error) {
       console.error("Error:", error);
-      res.status(500).json({ message: "An error occurred." });
+      res.status(500).json({ message: "An error occurred." + error });
     }
   }
   
