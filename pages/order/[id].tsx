a6f7fe495f60df80bd8fb45a6f38713b1c85a7d8
Abhindev <abhindev4432@gmail.com>
2023-03-28 10:30:34 +0530

    error

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -10,17 +10,20 @@ interface OrderProps {
         status: number;
       }
     | any;
-  chash: any;
+    error: any;
 }
 
-const Order = ({ order }: OrderProps) => {
-  console.log(order);
+const Order = ({ order, error }: OrderProps) => {
+  console.log(error);
   // console.log(chash);
 
   return (
     <div>
+      {error==null? <h1>error</h1>:
+      <>
       <h1>hay</h1>
-      <h1>{order._id}</h1>
+     <h1>{order._id}</h1>
+     </>}
     </div>
   );
 };
@@ -28,47 +31,6 @@ const Order = ({ order }: OrderProps) => {
 interface Params {
   id: string;
 }
-
-// export const getServerSideProps = async ({ params }: { params: Params }) => {
-  
-//     const resone = await fetch(
-//       `${process.env.DOM}/api/order/chashfree/${params.id}`
-//     );
-//     const chash = await resone.json();
-//     const orderStatus = chash.data.order_status;
-//       console.log(orderStatus);
-
-//     const uri = process.env.MONGODB_URI;
-//     if (!uri) {
-//       throw new Error('MongoDB connection string is missing.');
-//     }
-//     const client = await MongoClient.connect(uri);
-//     const db = client.db('kalianiammas');
-    
-//     if(orderStatus=="PAID"){
-//       const orders = await db
-//       .collection('orders')
-//       .findOne({ _id: new ObjectId(params.id) });
-//       const order = JSON.parse(JSON.stringify(orders))
-//       const status = (order.status);
-//       if (status < 1 ){
-//         console.log("true")
-//         // Update the order with method: 1
-//         const updatedOrder = await db.collection('orders').updateOne(
-//           { _id: new ObjectId(params.id) },
-//           { $set: { status: 1 } }
-//         );
-//       }
-//       return {
-//         props: {
-//           order: order,
-//         },
-//       };
-//     } else {
-//       const deletOrder = await db.collection('orders').deleteOne(
-//       { _id: new ObjectId(params.id) })
-//     }
-// };
 
 export default Order;
 
@@ -114,9 +76,9 @@ export const getServerSideProps = async ({ params }: { params: Params }) => {
     }
   } catch (err:any) {
     console.log(err);
-    return {
+    return { 
       props: {
-        error: err.message,
+        error: null,
       },
     };
   }
