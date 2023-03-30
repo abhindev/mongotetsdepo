bdcddb9a2a8e85806e921a8e67bde5b8b38e12da
Abhindev <abhindev4432@gmail.com>
2023-03-30 15:33:16 +0530

    clear butto tes

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -47,6 +47,7 @@ function Cart() {
   console.log(redux.total);
   return (
     <div className={width>600 ?styles.cart :styles.mob}>
+      <button onClick={()=>clear()}>clear</button>
       {width>600 ?<div className={styles.container}>
         <div className={styles.left}>
           {ckeckout==true ? <CheckOut />:<CartItem cart={redux} />}
