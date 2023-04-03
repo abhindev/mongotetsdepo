8f3c941fe6a40b707ec6d7625d250d068f98fe4b
Abhindev <abhindev4432@gmail.com>
2023-04-03 23:46:22 +0530

    loading

---
+++ pages/product/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
