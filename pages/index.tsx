eaebb3a9fb044afd8d706863c76904a8d6ac9a47
Abhindev <abhindev4432@gmail.com>
2023-04-05 01:10:25 +0530

    head color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import { InferGetServerSidePropsType } from 'next'
-import styles from "../styles/Home.module.css"
-import Featured from "../components/tools/Featured"
-import PoductList from "../components/template/products/productlist"
-
+import Head from "next/head";
+import clientPromise from "../lib/mongodb";
+import { InferGetServerSidePropsType } from "next";
+import styles from "../styles/Home.module.css";
+import Featured from "../components/tools/Featured";
+import PoductList from "../components/template/products/productlist";
 
 export async function getServerSideProps() {
   try {
     const client = await clientPromise;
     const db = client.db("kalianiammas");
-    
+
     const Data = await db.collection("products").find({}).toArray();
-    const properties = JSON.parse(JSON.stringify(Data))
+    const properties = JSON.parse(JSON.stringify(Data));
     return {
       props: { products: properties },
-    }
+    };
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 }
 
@@ -27,13 +26,19 @@ export default function Home({
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create App</title>
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- Chrome, Firefox OS and Opera --> */}
+        <meta name="theme-color" content="#77a31f" />
+        {/* <!-- Windows Phone --> */}
+        <meta name="msapplication-navbutton-color" content="#77a31f" />
+        {/* <!-- iOS Safari --> */}
+        <meta name="apple-mobile-web-app-status-bar-style" content="#77a31f" />
       </Head>
       <main>
-        <Featured/>
-      <PoductList products={products}/>
+        <Featured />
+        <PoductList products={products} />
       </main>
-    </div> 
-  )
+    </div>
+  );
 }
