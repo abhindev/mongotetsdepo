058529cc854e8942b6fccfc311732039519ebcc9
Abhindev <abhindev4432@gmail.com>
2023-04-12 11:53:45 +0530

    uususususus

--- pages/api/order/index.js
+++ pages/api/order/index.js
@@ -5,16 +5,17 @@ const handler = async (req, res) => {
   const { method } = req;
 
   const client = await clientPromise;
-  const db = client.db();
+  const db = client.db("");
 
   if (method === "GET") {
     try {
-      const orders = await db.collection("orders").find().toArray();
-      res.status(200).json(orders, insertedId);
+      const users = await db.collection("users").find({ "phoneNumber": "+916235354432" }).toArray();
+      res.status(200).json(users);
     } catch (err) {
       res.status(500).json(err);
     }
   }
+  
   if (method === "POST") {
     try {
       const order = await db.collection("orders").insertOne(req.body);
