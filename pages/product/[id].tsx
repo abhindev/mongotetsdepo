92d1ebb3ddd2e2eec3724d6abaa14cc610cc7bc4
Abhindev <abhindev4432@gmail.com>
2023-05-18 14:01:18 +0530

    new 222

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -100,26 +100,29 @@ const Product = ({ product, products }: any) => {
   }, [showDiv]);
   return (
     <>
-      <head><script>
+    <head>
+     <script>
     {`!function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
     if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
     n.queue=[];t=b.createElement(e);t.async=!0;
     t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-     fbq('init', '164880206511230');
+    fbq('init', '452105277009480');
     fbq('track', 'PageView');`}
   </script>
   <noscript>
     <img
       height="1"
       width="1"
+      style={{ display: 'none' }}
       alt="fb-pixel"
-      src="https://www.facebook.com/tr?id=164880206511230&ev=PageView&noscript=1"
+      src="https://www.facebook.com/tr?id=452105277009480&ev=PageView&noscript=1"
     />
-  </noscript></head>
+  </noscript>
+     </head>
       <div className={styles.container}>
         <div className={styles.left}>
           <div className={styles.imgContainer}>
